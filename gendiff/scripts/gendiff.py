fca3de9fb1e639596ec3e8cefc466202bf2b19f4
Vera Dobrodeeva <dobro.vera@gmail.com>
2022-10-21 14:49:52 +0300

    1st step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,10 +7,9 @@ import argparse
 def main():
     """display help information"""
     parser = argparse.ArgumentParser(prog='gendiff', description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', metavar = 'first_file')
-    parser.add_argument('second_file', metavar = 'second_file')
-    args = parser.parse_args()
-    print(args.accumulate(args.integers))
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.print_help()
 
 if __name__ == '__main__':
     main()
