c0f679b451383fdf8d7f00f079f017adf2d84bce
Vera Dobrodeeva <dobro.vera@gmail.com>
2022-11-23 15:46:53 +0600

    fix linter errors

--- gendiff/argument_parser.py
+++ gendiff/argument_parser.py
@@ -5,10 +5,13 @@ import argparse
 
 def argument_parser():
     """display help information"""
-    parser = argparse.ArgumentParser(prog='gendiff', description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(prog='gendiff',
+                                     description='Compares two '
+                                                 'configuration '
+                                                 'files and shows a '
+                                                 'difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     return args
-
