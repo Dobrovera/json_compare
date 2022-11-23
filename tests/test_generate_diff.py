19a477897fad48bdb2637fbcb2f9727692a0b107
Vera Dobrodeeva <dobro.vera@gmail.com>
2022-11-23 21:17:09 +0600

    make test

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -3,11 +3,11 @@ from gendiff.generate_diff import generate_diff
 
 
 def test_gendiff_json():
-    file1 = path.join(path.dirname("./fixtures/"),
+    file1 = path.join(path.dirname("./tests/fixtures/"),
                       "test_file1.json")
-    file2 = path.join(path.dirname("./fixtures/"),
+    file2 = path.join(path.dirname("./tests/fixtures/"),
                       "test_file2.json")
-    answer_str = str(path.join(path.dirname("./fixtures/"),
+    answer_str = str(path.join(path.dirname("./tests/fixtures/"),
                                "test_answer_json.txt"))
     with open(answer_str) as answer:
         assert generate_diff(file1, file2) == answer.read()
